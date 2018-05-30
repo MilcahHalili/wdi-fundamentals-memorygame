01afe90cb6c32da489070a14b034fa7752f9eb76
Milcah Centeno <milcahcenteno@Milcahs-MacBook-Air.local>
2018-05-30 00:27:58 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,12 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
-console.log("User flipped" + cardOne);
-console.log("User flipped" + cardTwo);
+var cards = ["queen", "king", "queen", "king"];
+var cardsInPlay = []
+var cardOne = cards[0];
+cardsInPlay.push("queen");
+console.log("User flipped queen")
+var cardTwo = cards[1];
+cardsInPlay.push("king");
+console.log("User flipped king")
+if (cardsInPlay.length === 2 && cardsInPlay[0] != cardsInPlay[1]) {
+	alert("Sorry, try again.")
+}
